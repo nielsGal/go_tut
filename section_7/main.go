acaf462f6f32caae59d659268a3b7c5642bcf074
nielsgal <galjaardniels@gmail.com>
2019-11-27 23:17:41 +0100

    finished exercise five

--- section_7/main.go
+++ section_7/main.go
@@ -9,4 +9,5 @@ func main() {
 	//%T type, %d base 10 %b binary %o octal, %x hex, %#x hex with 0x
 	x := fmt.Sprintf("%T %d %b %o %x %#x\n", y, y, y, y, y, y)
 	fmt.Print(x)
+	fmt.Printf("%v \n", y) //the default value always
 }
