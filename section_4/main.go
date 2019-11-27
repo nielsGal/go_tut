381da61d2f65bf5c3dc1e8ad25a872ea89d599a2
nielsgal <galjaardniels@gmail.com>
2019-11-27 22:22:20 +0100

    excercise two

--- section_4/main.go
+++ section_4/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	x := 42 //first time use walrus
 	fmt.Println(x)
-	x = 19 //second time use normal operator
+	x = 18 //second time use normal operator
 	fmt.Println(x)
+	y := x + 24 // walrus can contain vars
+	fmt.Println(y)
 }
