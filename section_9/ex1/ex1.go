7ebe42555f969d9574d22e65ae99a56c9af6eff2
nielsgal <galjaardniels@gmail.com>
2019-11-27 23:43:01 +0100

    progress on prac sess 1

---
+++ section_9/ex1/ex1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "james bond"
+	z := true
+	fmt.Println(x, y, z)
+	fmt.Print(x, " ")
+	fmt.Print(y, " ")
+	fmt.Print(z, "\n")
+}
---
+++ section_9/ex2/ex2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var x = 42
+var y = "james bond"
+var z = true
+
+func main() {
+	fmt.Println(x, y, z)
+}
