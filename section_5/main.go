0daf620fa564b67f6311605244f2105f66b9cf54
nielsgal <galjaardniels@gmail.com>
2019-11-27 22:37:03 +0100

    finished exercise three

--- section_5/main.go
+++ section_5/main.go
@@ -5,6 +5,7 @@ import (
 )
 
 var a int = 25 //a global cannot use the walrus (:=)
+var b int      // some zero value exists for all types
 
 func main() {
 	var x int = 42 //var identifier type,
@@ -12,4 +13,5 @@ func main() {
 	var y, z int = 22, 23 //mutli assignment
 	fmt.Println(y, " ", z)
 	fmt.Println(a) // this one is global, it is not checked for use.
+	fmt.Println(b)
 }
