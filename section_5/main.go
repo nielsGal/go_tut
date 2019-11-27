60aed8a79f7836a86c6b1d4a521e8438f415be60
nielsgal <galjaardniels@gmail.com>
2019-11-27 22:31:39 +0100

    exercise three

--- section_5/main.go
+++ section_5/main.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
+var a int = 25 //a global cannot use the walrus (:=)
+
 func main() {
 	var x int = 42 //var identifier type,
 	fmt.Println(x)
 	var y, z int = 22, 23 //mutli assignment
 	fmt.Println(y, " ", z)
+	fmt.Println(a) // this one is global, it is not checked for use.
 }
