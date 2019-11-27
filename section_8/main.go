eb33456494bb42ab2241bb1cdafa4508d18d30fb
nielsgal <galjaardniels@gmail.com>
2019-11-27 23:27:11 +0100

    exercise six

--- section_8/main.go
+++ section_8/main.go
@@ -13,8 +13,13 @@ type specialInt = int
 var c specialInt
 
 func main() {
+	b = 52
+	c = 43
 	fmt.Printf("%T\n", a)
 	fmt.Printf("%T\n", b)
 	fmt.Printf("%T\n", c)
+	// cannot do a = b
+	a = c // this is clearly possible
+	fmt.Println(a)
 
 }
