3a4caec8e45225f1cc170a63c9ffc4dccf22d954
nielsgal <galjaardniels@gmail.com>
2019-11-28 18:47:50 +0100

    progress of  chapter 4

--- chapter_4/section_1/main.go
+++ chapter_4/section_1/main.go
@@ -6,7 +6,9 @@ import "fmt"
 
 func main() {
 	var x [5]int   //an array of size 5
+	var y [6]int   // another arrat of size 6
 	fmt.Println(x) //we can just print these unlike java
 	x[3] = 42      //0 based indexing like normal
 	fmt.Println(x)
+	fmt.Printf("%T %T \n", x, y) //the types are disticnt [5]int vs [6]int
 }
--- chapter_4/section_2/main.go
+++ chapter_4/section_2/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//x := type{values}
+	x := []int{12, 23, 34, 45, 56}
+	fmt.Printf("%T %v\n", x, x) //this does not have type [5]int but type []int
+	for _, value := range x {   //throw away key pring value
+		fmt.Println(value)
+	}
+	for key, _ := range x {
+		fmt.Println(key)
+	}
+	for key := range x { //this is preferable
+		fmt.Println(key)
+	}
 }
