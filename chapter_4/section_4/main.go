2f6c70e39d8302ce758224c3eefd42eefd1a2af9
nielsgal <galjaardniels@gmail.com>
2019-11-28 19:13:11 +0100

    progress on chapter 4

--- chapter_4/section_4/main.go
+++ chapter_4/section_4/main.go
@@ -6,4 +6,15 @@ func main() {
 	x := []int{1, 23, 45, 7, 8, 9, 10}
 	x = append(x[:2], x[4:]...) //delete numbers x[3] x[4]
 	fmt.Println(x)
+	y := make([]int, 10, 12)
+	fmt.Printf("%T %v\n", y, y)
+	//y[11] = 40   this will error
+	y = append(y, 12)
+	fmt.Println(cap(y))
+	y = append(y, 12)
+	fmt.Println(cap(y))
+	y = append(y, 12) //this one will double the cap
+	fmt.Println(cap(y))
+
+	fmt.Printf("%T %v\n", y, y)
 }
