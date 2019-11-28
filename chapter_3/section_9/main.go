5ef24fd9e97ce08290be03a560852a641db9e790
nielsgal <galjaardniels@gmail.com>
2019-11-28 15:41:06 +0100

    progress on chapter 3

--- chapter_3/section_9/main.go
+++ chapter_3/section_9/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Printf("true && true %v\n", true && true)
+	fmt.Printf("true && false %v\n", true && false)
+	fmt.Printf("!true %v\n", !true)
+	fmt.Printf("!false %v\n", !false)
+	fmt.Printf("true || true %v\n", true || true)
 }
