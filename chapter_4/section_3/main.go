bf9ffd648424c949a5b4c961bb361595498e8119
nielsgal <galjaardniels@gmail.com>
2019-11-28 18:55:38 +0100

    progress on chapter 4

---
+++ chapter_4/section_3/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{42, 43, 44, 45, 46}
+	fmt.Println(x[1:3])              //take a slice of the slice using python like indexing
+	for key, value := range x[1:3] { //the key is simply zero starting at the lower bound
+		fmt.Println(key, value)
+	}
+	x = append(x, 6, 7, 8, 9) //this is a static function, not part of x
+	for key := range x {
+		fmt.Println(key)
+	}
+}
