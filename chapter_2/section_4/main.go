17c08db59e094861046d8b12923433d5cf29c05f
nielsgal <galjaardniels@gmail.com>
2019-11-28 12:51:19 +0100

    finished chapter two exercise four

--- chapter_2/section_4/main.go
+++ chapter_2/section_4/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := "a string"
+	y := `"a raw string"`
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	bs := []byte(x)
+	fmt.Printf("%v %s %T\n", bs, bs, bs)
+
+	for i := 0; i < len(x); i++ {
+		fmt.Printf("%#U", x[i])
+	}
+	for i, v := range x {
+		println(i, v)
+	}
 }
