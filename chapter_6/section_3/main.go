26c23f1487a7819a9fce7a3e43a0271023944871
nielsgal <galjaardniels@gmail.com>
2019-11-28 23:53:25 +0100

    progress of chapter 6

--- chapter_6/section_3/main.go
+++ chapter_6/section_3/main.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	x := sum(1, 2, 3, 4, 5, 6, 8)
+	xi := []int{1, 2, 3, 4, 5, 6, 8}
+	x := sum(xi...)
 	fmt.Println(x)
+	y := sum()
+	fmt.Println(y)
 }
 
 func sum(x ...int) int {
-	fmt.Printf("%T , %v \n", x, x)
-
 	sum := 0
 	for _, v := range x {
 		sum += v
