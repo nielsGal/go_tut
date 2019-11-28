395acf45cdd3d909c09c6ce85c2f2e9c3f4aa299
nielsgal <galjaardniels@gmail.com>
2019-11-28 20:18:33 +0100

    progress on chapter 4

--- chapter_4/section_5/main.go
+++ chapter_4/section_5/main.go
@@ -10,4 +10,29 @@ func main() {
 
 	xp := [][]string{jb, mp}
 	fmt.Println(xp)
+	m := map[string]int{
+		"james":      42,
+		"moneypenny": 27,
+	}
+	fmt.Println(m["james"])  //value in map
+	fmt.Println(m["james2"]) //value not in map return the 0
+	if v, ok := m["james2"]; ok {
+		println(v)
+	}
+	if v, ok := m["james"]; ok {
+		println(v)
+	}
+	m["james2"] = 34
+	if v, ok := m["james2"]; ok { //now we do have the value
+		println(v)
+	}
+	for key, value := range m {
+		fmt.Println(key, value)
+	}
+	delete(m, "james")
+	if v, ok := m["james"]; ok { // now james is gone
+		println(v)
+	} else {
+		println(ok)
+	}
 }
