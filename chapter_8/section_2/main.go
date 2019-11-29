20a8a098033104bfea210ab7dcd44bce0f3bc913
nielsgal <galjaardniels@gmail.com>
2019-11-29 16:38:38 +0100

    progress of chapter 8

---
+++ chapter_8/section_2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First, Last string
+	Age         int
+}
+
+func main() {
+	p1 := person{
+		First: "james",
+		Last:  "bond",
+		Age:   32,
+	}
+	p2 := person{
+		First: "miss",
+		Last:  "moneypenny",
+		Age:   27,
+	}
+	people := []person{p1, p2}
+
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(people)
+	fmt.Println(string(bs))
+	var dest []person
+	err = json.Unmarshal(bs, &dest)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(dest)
+}
