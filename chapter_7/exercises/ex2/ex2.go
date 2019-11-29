5128490e3f5a4189cf5eb5707999fa554b57c74a
nielsgal <galjaardniels@gmail.com>
2019-11-29 12:48:11 +0100

    finished chapter seven

--- chapter_7/exercises/ex2/ex2.go
+++ chapter_7/exercises/ex2/ex2.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	first, last string
+	age         int
+}
+
+func main() {
+	p1 := person{
+		first: "james",
+		last:  "bond",
+		age:   32,
+	}
+
+	fmt.Println(p1)
+	changeMe(&p1)
+	fmt.Println(p1)
+}
+
+func changeMe(pptr *person) {
+	pptr.first = "a new first name"
 }
